f2e42704f0ca64166c28550981697163f7a37341
Kittors <364299311@qq.com>
2024-12-18 22:04:45 +0800

    Refactor dark mode switch component to enhance theme transition effects. Updated theme toggling logic in `useTheme` hook for smoother transitions and added view transition animations in styles. The `SwitchDark` component now utilizes a click event to trigger theme changes with visual feedback.

--- src/components/SwitchDark/index.vue
+++ src/components/SwitchDark/index.vue
@@ -4,17 +4,58 @@
     inline-prompt
     :active-icon="Sunny"
     :inactive-icon="Moon"
-    @change="switchDark"
+    @click.stop="toggleTheme"
+    ref="switchRef"
   />
 </template>
 
 <script setup lang="ts">
-defineOptions({
-  name: 'SwitchDarkMode',
-})
+import { ref } from 'vue'
 import { useTheme } from '@/hooks/useTheme'
 import { useAppStore } from '@/stores/modules/app'
 import { Sunny, Moon } from '@element-plus/icons-vue'
+import type { ComponentPublicInstance } from 'vue'
+
+defineOptions({
+  name: 'SwitchDarkMode',
+})
+
+const switchRef = ref<ComponentPublicInstance>()
 const { switchDark } = useTheme()
 const globalStore = useAppStore()
+
+const toggleTheme = (event: MouseEvent) => {
+  const switchEl = switchRef.value?.$el
+  const rect = switchEl?.getBoundingClientRect()
+  const x = rect ? rect.left + rect.width / 2 : event.clientX
+  const y = rect ? rect.top + rect.height / 2 : event.clientY
+
+  const endRadius =
+    Math.hypot(Math.max(x, window.innerWidth - x), Math.max(y, window.innerHeight - y)) * 1.2
+
+  if (!document.startViewTransition) {
+    return
+  }
+  switchDark()
+  const transition = document.startViewTransition(async () => {
+    switchDark()
+  })
+
+  transition.ready.then(() => {
+    const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${endRadius}px at ${x}px ${y}px)`]
+
+    document.documentElement.animate(
+      {
+        clipPath: !globalStore.isDark ? clipPath : [...clipPath].reverse(),
+      },
+      {
+        duration: 600,
+        easing: 'cubic-bezier(0.33, 1, 0.68, 1)',
+        pseudoElement: !globalStore.isDark
+          ? '::view-transition-new(root)'
+          : '::view-transition-old(root)',
+      },
+    )
+  })
+}
 </script>
